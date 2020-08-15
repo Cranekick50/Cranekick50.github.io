9d58a125e86e039b464dd7a12c13077d44d8aeb0
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-14 19:16:29 -0500

    Updates

---
+++ precache-manifest.d6b6116319cfaaa9a80f227aa7c5006a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e0ac911ccc96369a89062cb371afdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df62c8741d351e65663",
+    "url": "/static/css/main.1adab62a.chunk.css"
+  },
+  {
+    "revision": "699ae0e8799dbfd60e6d",
+    "url": "/static/js/2.c794c759.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c794c759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df62c8741d351e65663",
+    "url": "/static/js/main.f8044c8b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7950445e0450c47549ead57e48cb92f0",
+    "url": "/static/media/profile.7950445e.jpg"
+  },
+  {
+    "revision": "3042aab8cbfc2061a57e10e316e34d6e",
+    "url": "/static/media/scheduler.3042aab8.JPG"
+  },
+  {
+    "revision": "a922041c846ef6bf315b66efd4ad34c4",
+    "url": "/static/media/teamAPI.a922041c.jpg"
+  },
+  {
+    "revision": "91d372e9ccd083e715ed39600965866a",
+    "url": "/static/media/weatherAPI.91d372e9.JPG"
+  }
+]);
